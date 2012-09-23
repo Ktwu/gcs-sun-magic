23ed5400e5f9a11428ca848aa2017727336d9e9c
ayuen <amosyuen@gmail.com>
2012-09-23 17:14:39 -0400

    Ignored the debug and ipch folders Changed tab spacing of starter files.

--- Sun Magic/Game.h
+++ Sun Magic/Game.h
@@ -5,15 +5,21 @@
 class Game {
 
 public:
-  static void Start();
+	static void Start();
 
 private:
-  static bool IsExiting();
-  static void GameLoop();
+	static bool IsExiting();
+	static void GameLoop();
 
-  enum GameState { Uninitialized, ShowingSplash, Paused, 
-          ShowingMenu, Playing, Exiting };
+	enum GameState {
+		Uninitialized,
+		ShowingSplash,
+		Paused, 
+		ShowingMenu,
+		Playing,
+		Exiting
+	};
   
-  static GameState _gameState;
-  static sf::RenderWindow _mainWindow;
+	static GameState _gameState;
+	static sf::RenderWindow _mainWindow;
 };
--- Sun Magic/Sun Magic.cpp
+++ Sun Magic/Sun Magic.cpp
@@ -7,48 +7,48 @@
 
 // TODO delete this once everyone's verified it works
 void zinniaTest() {
-	  zinnia::Recognizer *recognizer = zinnia::Recognizer::create();
-  if (!recognizer->open("zinnia/models/handwriting-ja.model")) {
-    std::cout << "ERROR: " << recognizer->what() << std::endl;
-    return;
-  }
+	zinnia::Recognizer *recognizer = zinnia::Recognizer::create();
+	if (!recognizer->open("zinnia/models/handwriting-ja.model")) {
+		std::cout << "ERROR: " << recognizer->what() << std::endl;
+		return;
+	}
 
-  zinnia::Character *character = zinnia::Character::create();
-  character->clear();
-  character->set_width(300);
-  character->set_height(300);
-  character->add(0, 51, 29);
-  character->add(0, 117, 41);
-  character->add(1, 99, 65);
-  character->add(1, 219, 77);
-  character->add(2, 27, 131);
-  character->add(2, 261, 131);
-  character->add(3, 129, 17);
-  character->add(3, 57, 203);
-  character->add(4, 111, 71);
-  character->add(4, 219, 173);
-  character->add(5, 81, 161);
-  character->add(5, 93, 281);
-  character->add(6, 99, 167);
-  character->add(6, 207, 167);
-  character->add(6, 189, 245);
-  character->add(7, 99, 227);
-  character->add(7, 189, 227);
-  character->add(8, 111, 257);
-  character->add(8, 189, 245);
+	zinnia::Character *character = zinnia::Character::create();
+	character->clear();
+	character->set_width(300);
+	character->set_height(300);
+	character->add(0, 51, 29);
+	character->add(0, 117, 41);
+	character->add(1, 99, 65);
+	character->add(1, 219, 77);
+	character->add(2, 27, 131);
+	character->add(2, 261, 131);
+	character->add(3, 129, 17);
+	character->add(3, 57, 203);
+	character->add(4, 111, 71);
+	character->add(4, 219, 173);
+	character->add(5, 81, 161);
+	character->add(5, 93, 281);
+	character->add(6, 99, 167);
+	character->add(6, 207, 167);
+	character->add(6, 189, 245);
+	character->add(7, 99, 227);
+	character->add(7, 189, 227);
+	character->add(8, 111, 257);
+	character->add(8, 189, 245);
 
-  zinnia::Result *result = recognizer->classify(*character, 10);
-  if (!result) {
-     std::cout << "ERROR: " << recognizer->what() << std::endl;
-     return;
-  }
-  for (size_t i = 0; i < result->size(); ++i) {
-    std::cout << result->value(i) << "\t" << result->score(i) << std::endl;
-  }
-  delete result;
+	zinnia::Result *result = recognizer->classify(*character, 10);
+	if (!result) {
+		std::cout << "ERROR: " << recognizer->what() << std::endl;
+		return;
+	}
+	for (size_t i = 0; i < result->size(); ++i) {
+		std::cout << result->value(i) << "\t" << result->score(i) << std::endl;
+	}
+	delete result;
 
-  delete character;
-  delete recognizer;
+	delete character;
+	delete recognizer;
 }
 
 
