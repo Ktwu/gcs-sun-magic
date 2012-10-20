4f8493ed07fa5959b26f62be2282646ecfbb3312
ajeun <ajeunyuen@gmail.com>
2012-10-19 23:28:29 -0400

    -Added SFML 2.0 character_tile versions -Added main.cpp to project solution

--- Sun Magic/character_tile.cpp
+++ Sun Magic/character_tile.cpp
@@ -1,12 +1,8 @@
 #include "stdafx.h"
 #include "character_tile.h"
 
-#include <fstream>
-#include <iostream>
-
+#include <iterator>
 #include "time.h"
-
-#include "sfm.h"
 
 namespace SunMagic {
 	
@@ -19,10 +15,10 @@ void CharacterTile::InitRecognizer(const char *modelFile) {
 	}
 }
 
-sf::Unicode::UTF32String CharacterTile::UTF8ToUTF32(const char* utf8str) {
-	std::basic_string<sf::Uint32> utf32str;
-	sf::Unicode::UTF8ToUTF32((unsigned char*)utf8str, (unsigned char*)utf8str + strlen(utf8str), back_inserter(utf32str));
-	return utf32str;
+sf::String CharacterTile::UTF8ToUTF32(const char* utf8str) {
+	sf::Uint32 utf32str[] = {0,0};
+	sf::Utf8::toUtf32((unsigned char*)utf8str, (unsigned char*)utf8str + strlen(utf8str), utf32str);
+	return sf::String(utf32str);
 }
 
 CharacterTile::CharacterTile(float x, float y, size_t width, size_t height) :
@@ -30,16 +26,16 @@ CharacterTile::CharacterTile(float x, float y, size_t width, size_t height) :
 	_character(zinnia::Character::create()),
 	_unicode(),
 	_currentStroke(0),
-	_guideCharacter(NULL),
-	_guideUnicode(),
-	_animationStroke(-1),
+	_traceCharacter(NULL),
+	_traceUnicode(),
+	_animatingStroke(-1),
 	_animationSpeed(140.0f),
 	_waitSeconds(0.2f),
 	_strokeThickness(4),
 	_borderColor(sf::Color(80, 80, 80)),
 	_strokeColor(sf::Color::Black),
-	_guideLineColor(sf::Color(238, 238, 238)),
-	_guideColor(sf::Color(210, 210, 210)),
+	_guideColor(sf::Color(238, 238, 238)),
+	_traceColor(sf::Color(210, 210, 210)),
 	_animateColor(sf::Color(150, 150, 150)),
 	_secondsToWait(0),
 	_currentLineDistance(0)
@@ -47,6 +43,8 @@ CharacterTile::CharacterTile(float x, float y, size_t width, size_t height) :
 	_character->clear();
 	_character->set_width(width);
 	_character->set_height(height);
+	
+	_strokeLines.push_back(std::vector<sf::RectangleShape>());
 }
 
 CharacterTile::~CharacterTile() {
@@ -56,22 +54,32 @@ CharacterTile::~CharacterTile() {
 zinnia::Character * CharacterTile::GetCharacter() {
 	return _character;
 }
-sf::Unicode::UTF32String CharacterTile::GetUnicode() {
+sf::String CharacterTile::GetUnicode() {
 	return _unicode;
 }
 
 size_t CharacterTile::NumStrokes() {
 	return _character->strokes_size();
 }
-void CharacterTile::AddStrokePoint(float x, float y) {
-	_character->add(_currentStroke, (int)(x - _position.x), (int)(y - _position.y));
+void CharacterTile::AddStrokePoint(sf::Vector2f p) {
+	p -= _position;
+	_character->add(_currentStroke, (int)p.x, (int)p.y);
+	size_t points = _character->stroke_size(_currentStroke);
+	if (points > 1) {
+		sf::RectangleShape rect;
+		sf::Vector2f prev((float)_character->x(_currentStroke, points-2),
+			(float)_character->y(_currentStroke, points-2));
+		sf::SetToLine(rect, prev, p, _strokeThickness);
+		rect.setFillColor(_strokeColor);
+		_strokeLines.back().push_back(rect);
+	}
 }
 // Consider every two consecutive lines. If the angle between the lines is small enough remove the mid point.
 size_t SmoothPoints(std::vector<sf::Vector2f>& points1, std::vector<sf::Vector2f>& points2, size_t index) {
-	static const float THRESHOLD_ANGLE = cos(DEGTORAD(15));
+	static const float THRESHOLD_ANGLE = cos(sfm::DEGTORAD(15));
 
-	size_t removed = 0;
 	size_t i;
+	size_t removed = 0;
 	for (i = 0; i < index; i++) {
 		points2.push_back(points1[i]);
 	}
@@ -98,6 +106,9 @@ size_t SmoothPoints(std::vector<sf::Vector2f>& points1, std::vector<sf::Vector2f
 	return removed;
 }
 void CharacterTile::EndStroke() {
+	// Remove all lines of the last stroke
+	_strokeLines.pop_back();
+
 	// Copy points except for last stroke
 	size_t strokes =_character->strokes_size();
 	zinnia::Character *character = zinnia::createCharacter();
@@ -136,15 +147,22 @@ void CharacterTile::EndStroke() {
 	delete _character;
 	_character = character;
 
+	// Add lines for the new stroke
+	CreateLines(_strokeLines, _character, _strokeColor, _currentStroke, _currentStroke+1);
+	_strokeLines.push_back(std::vector<sf::RectangleShape>());
+
 	_currentStroke++;
 	Reclassify();
 }
 void CharacterTile::UndoStroke() {
 	if (_currentStroke < 0)
 		return;
-
+	
 	// There is no remove stroke, so we just create a copy up to the last stroke.
-	size_t strokes =_character->strokes_size();
+	size_t strokes =_character->strokes_size();	
+	if (_currentStroke == 0 && strokes == 0)
+		return;
+
 	zinnia::Character *character = zinnia::createCharacter();
 	character->set_width(_character->width());
 	character->set_height(_character->height());
@@ -161,36 +179,42 @@ void CharacterTile::UndoStroke() {
 	if (_currentStroke == strokes) {
 		_currentStroke--;
 		Reclassify();
+		_strokeLines.pop_back();
+	} else {
 	}
+	_strokeLines.back().clear();
 }
 void CharacterTile::Clear() {
 	_character->clear();
 	_currentStroke = 0;
+	_strokeLines.clear();
+	_strokeLines.push_back(std::vector<sf::RectangleShape>());
 	Reclassify();
 }
 float CharacterTile::GetStrokeError(size_t stroke) {
-	if (stroke >= _character->strokes_size() || _guideCharacter == NULL ||
-		stroke >= _guideCharacter->strokes_size()) {
+	if (stroke >= _character->strokes_size() || _traceCharacter == NULL ||
+		stroke >= _traceCharacter->strokes_size()) {
 			throw "Invalid stroke";
 	}
 
 	size_t i = 0;
 	size_t j = 0;
 	size_t points = _character->stroke_size(stroke);
-	size_t guide_points = _guideCharacter->stroke_size(stroke);
-	if (points < 2 || guide_points < 2) {
+	size_t trace_points = _traceCharacter->stroke_size(stroke);
+	if (points < 2 || trace_points < 2) {
 		// Not enough points to calculate error
 		return 0;
 	}
 
 	float error = 0;
-	sf::Vector2f p1 = sf::Vector2f(_character->x(stroke, 0), _character->y(stroke, 0));
-	sf::Vector2f p2 = sf::Vector2f(_character->x(stroke, 1), _character->y(stroke, 1));
-	sf::Vector2f gp1 = sf::Vector2f(_guideCharacter->x(stroke, 0), _guideCharacter->y(stroke, 0));
-	sf::Vector2f gp2 = sf::Vector2f(_guideCharacter->x(stroke, 1), _guideCharacter->y(stroke, 1));
+	sf::Vector2i p1 = sf::Vector2i(_character->x(stroke, 0), _character->y(stroke, 0));
+	sf::Vector2i p2 = sf::Vector2i(_character->x(stroke, 1), _character->y(stroke, 1));
+	sf::Vector2i gp1 = sf::Vector2i(_traceCharacter->x(stroke, 0), _traceCharacter->y(stroke, 0));
+	sf::Vector2i gp2 = sf::Vector2i(_traceCharacter->x(stroke, 1), _traceCharacter->y(stroke, 1));
 	while (i < points) {
 		// Find the distance from the 
 	}
+	return 0;
 }
 void CharacterTile::Resize(size_t width, size_t height) {
 	size_t curr_width = _character->width();
@@ -199,7 +223,7 @@ void CharacterTile::Resize(size_t width, size_t height) {
 	zinnia::Character *character = zinnia::createCharacter();
 	character->set_width(width);
 	character->set_height(height);
-	for (size_t i = 0; i < _currentStroke; i++) {
+	for (int i = 0; i < _currentStroke; i++) {
 		size_t points = _character->stroke_size(i);
 		for (size_t j = 0; j < points; j++) {
 			character->add(i, _character->x(i, j) * width / curr_width, _character->y(i, j) * height / curr_height);
@@ -207,36 +231,44 @@ void CharacterTile::Resize(size_t width, size_t height) {
 	}
 }
 
-zinnia::Character * CharacterTile::GetGuideCharacter() {
-	return _guideCharacter;
+zinnia::Character * CharacterTile::GetTraceCharacter() {
+	return _traceCharacter;
 }
-sf::Unicode::UTF32String CharacterTile::GetGuideUnicode() {
-	return _guideUnicode;
+sf::String CharacterTile::GetTraceUnicode() {
+	return _traceUnicode;
 }
-void CharacterTile::SetGuideCharacter(zinnia::Character *character, sf::Unicode::UTF32String value) {
-	_guideCharacter = character;
-	_guideUnicode = value;
-	_animationStroke = -1;
+void CharacterTile::SetTraceCharacter(zinnia::Character *character, sf::String value) {
+	_traceCharacter = character;
+	_traceUnicode = value;
+	_animatingStroke = -1;
+	
+	_traceLines.clear();
+	if (_traceCharacter != NULL) {
+		CreateLines(_traceLines, _traceCharacter, _traceColor, 0, _traceCharacter->strokes_size());
+	}
 }
 
 bool CharacterTile::IsAnimating() {
-	return _animationStroke >= 0;
+	return _animatingStroke >= 0;
 }
 int CharacterTile::GetAnimationStroke() {
-	return _animationStroke;
+	return _animatingStroke;
 }
 void CharacterTile::SetAnimationStroke(int stroke) {
+	if (stroke == _animatingStroke)
+		return;
+
 	if (stroke >= 0) {
-		if (_guideCharacter == NULL)
-			throw "Guide character is NULL";
-		if (stroke >= _guideCharacter->strokes_size()) {
+		if (_traceCharacter == NULL)
+			throw "Trace character is NULL";
+		if (stroke >= (int)_traceCharacter->strokes_size()) {
 			stroke = -1;
 		}
 		_currentLineDistance = 0;
 		_secondsToWait = 0;
-		_animationStrokeLines.clear();
+		_animatingLines.clear();
 	}
-	_animationStroke = stroke;
+	_animatingStroke = stroke;
 }
 
 float CharacterTile::GetAnimationSpeed() {
@@ -267,18 +299,18 @@ void CharacterTile::SetBorderColor(sf::Color color) {
 	_borderColor = color;
 }
 
-sf::Color CharacterTile::GetGuideLineColor() {
-	return _guideLineColor;
-}
-void CharacterTile::SetGuideLineColor(sf::Color color) {
-	_guideLineColor = color;
-}
-
 sf::Color CharacterTile::GetGuideColor() {
 	return _guideColor;
 }
 void CharacterTile::SetGuideColor(sf::Color color) {
 	_guideColor = color;
+}
+
+sf::Color CharacterTile::GetTraceColor() {
+	return _traceColor;
+}
+void CharacterTile::SetTraceColor(sf::Color color) {
+	_traceColor = color;
 }
 
 sf::Color CharacterTile::GetAnimateColor() {
@@ -303,39 +335,40 @@ void CharacterTile::Update(float elapsedSeconds) {
 			if (_secondsToWait <= 0) {
 				// Reset animation stroke
 				_currentLineDistance = 0;
-				_animationStrokeLines.clear();
+				_animatingLines.clear();
 			}
 		} else {
 			_currentLineDistance += elapsedSeconds * _animationSpeed;
 
-			if (_animationStrokeLines.size() > 0) {
-				_animationStrokeLines.pop_back();
+			if (_animatingLines.size() > 0) {
+				_animatingLines.pop_back();
 			}
-			float x2 = _guideCharacter->x(_animationStroke, _animationStrokeLines.size());
-			float y2 = _guideCharacter->y(_animationStroke, _animationStrokeLines.size());
+			sf::RectangleShape rect;
+			rect.setFillColor(_animateColor);
+			sf::Vector2f p2((float)_traceCharacter->x(_animatingStroke, _animatingLines.size()),
+				(float)_traceCharacter->y(_animatingStroke, _animatingLines.size()));
 			while (true) {
-				if (_animationStrokeLines.size() + 1 >= _guideCharacter->stroke_size(_animationStroke)) {
+				if (_animatingLines.size() + 1 >= _traceCharacter->stroke_size(_animatingStroke)) {
 					// Finished animating the stroke
 					_secondsToWait = _waitSeconds;
 					break;
 				}
 
-				float x1 = x2;
-				float y1 = y2;
-				x2 = _guideCharacter->x(_animationStroke, _animationStrokeLines.size() + 1);
-				y2 = _guideCharacter->y(_animationStroke, _animationStrokeLines.size() + 1);
-				float dx = x2 - x1;
-				float dy = y2 - y1;
-				float distance = sqrt(dx * dx + dy * dy);
+				sf::Vector2f p1 = p2;
+				p2 = sf::Vector2f((float)_traceCharacter->x(_animatingStroke, _animatingLines.size() + 1),
+					(float)_traceCharacter->y(_animatingStroke, _animatingLines.size() + 1));
+				sf::Vector2f diff = p2 - p1;
+				float distance = sfm::Length(diff);
 				if (_currentLineDistance < distance) {
 					float ratio = _currentLineDistance / distance;
-					float x = x1 + ratio * (x2 - x1);
-					float y = y1 + ratio * (y2 - y1);
-					_animationStrokeLines.push_back(sf::Shape::Line(x1, y1, x, y, _strokeThickness, _animateColor));
+					sf::Vector2f p = p1 + ratio * diff;
+					sf::SetToLine(rect, p1, p, _strokeThickness);
+					_animatingLines.push_back(rect);
 					break;
 				} else {
 					_currentLineDistance -= distance;
-					_animationStrokeLines.push_back(sf::Shape::Line(x1, y1, x2, y2, _strokeThickness, _animateColor));
+					sf::SetToLine(rect, p1, p2, _strokeThickness);
+					_animatingLines.push_back(rect);
 				}
 			}
 		}
@@ -343,50 +376,85 @@ void CharacterTile::Update(float elapsedSeconds) {
 }
 
 void CharacterTile::Draw(sf::RenderWindow *mainWindow) {
-	const sf::View& view = mainWindow->GetView();
-	mainWindow->SetView(sf::View(view.GetHalfSize() - _position, view.GetHalfSize()));
+	sf::View view = mainWindow->getView();
+	sf::Vector2f center = view.getCenter();
+	mainWindow->setView(sf::View(center - _position, 2.f * center));
+	
+	float width = (float)_character->width();
+	float height = (float)_character->height();
 
-	float width = _character->width();
-	float height = _character->height();
 	// Draw background
-	mainWindow->Draw(sf::Shape::Rectangle(sf::Vector2f(0,0), sf::Vector2f(width, height), sf::Color::White));
+	sf::RectangleShape rect(sf::Vector2f(width, height));
+	rect.setFillColor(sf::Color::White);
+	mainWindow->draw(rect);
 
-	// Draw grid lines
-	float halfStroke = _strokeThickness / 2;
-	float positionWidth = width - halfStroke;
-	float positionHeight = height - halfStroke;
-	// Draw border
-	mainWindow->Draw(sf::Shape::Line(0, halfStroke, width, halfStroke, _strokeThickness, _borderColor));
-	mainWindow->Draw(sf::Shape::Line(0, height, width, height, _strokeThickness, _borderColor));
-	mainWindow->Draw(sf::Shape::Line(halfStroke, 0, halfStroke, height, _strokeThickness, _borderColor));
-	mainWindow->Draw(sf::Shape::Line(positionWidth, 0, positionWidth, height, _strokeThickness, _borderColor));
+	// Draw border lines
+	rect.setFillColor(_borderColor);
+	rect.setSize(sf::Vector2f(width, _strokeThickness));
+	rect.setPosition(0, 0);
+	mainWindow->draw(rect);
+	rect.setPosition(0, height - _strokeThickness);
+	mainWindow->draw(rect);
+	rect.setSize(sf::Vector2f(_strokeThickness, height));
+	rect.setPosition(0, 0);
+	mainWindow->draw(rect);
+	rect.setPosition(width - _strokeThickness, 0);
+	mainWindow->draw(rect);
+
 	// Draw guide lines
-	mainWindow->Draw(sf::Shape::Line(_strokeThickness, positionHeight/2, width - _strokeThickness, positionHeight/2, _strokeThickness, _guideLineColor));
-	mainWindow->Draw(sf::Shape::Line(positionWidth/2, _strokeThickness, positionWidth/2, height - _strokeThickness, _strokeThickness, _guideLineColor));
+	rect.setFillColor(_guideColor);
+	rect.setPosition(_strokeThickness, (height-_strokeThickness)/2);
+	rect.setSize(sf::Vector2f(width - 2*_strokeThickness, _strokeThickness));
+	mainWindow->draw(rect);
+	rect.setPosition((width-_strokeThickness)/2, _strokeThickness);
+	rect.setSize(sf::Vector2f(_strokeThickness, height - 2*_strokeThickness));
+	mainWindow->draw(rect);
 
-	// Draw guide character
-	if (_guideCharacter != NULL) {
-		// Draw stroke animation
-		if (IsAnimating()) {
-			DrawStrokes(mainWindow, _guideCharacter, _animateColor, 0, _animationStroke);
-			DrawStrokes(mainWindow, _guideCharacter, _guideColor, _animationStroke, _guideCharacter->strokes_size());
-			for (int i = 0; i < _animationStrokeLines.size(); i++) {
-				mainWindow->Draw(_animationStrokeLines[i]);
+	sf::CircleShape circle(0.5f * _strokeThickness);
+	circle.setOrigin(0.5f * _strokeThickness, 0.5f * _strokeThickness);
+	if (_traceCharacter != NULL) {
+		// Draw trace character up to the currently animated stroke, or all strokes if no animation
+		size_t maxStroke = (_animatingStroke < 0) ? _traceLines.size() : std::min((int)_traceLines.size(), _animatingStroke + 1);		
+		circle.setFillColor(_traceColor);
+		for (size_t i = 0; i < maxStroke; i++) {
+			circle.setPosition((float)_traceCharacter->x(i, 0), (float)_traceCharacter->y(i, 0));
+			mainWindow->draw(circle);
+			for (size_t j = 0; j < _traceLines[i].size(); j++) {
+				mainWindow->draw(_traceLines[i][j]);
+				circle.setPosition((float)_traceCharacter->x(i, j+1), (float)_traceCharacter->y(i, j+1));
+				mainWindow->draw(circle);
 			}
-		} else {
-			DrawStrokes(mainWindow, _guideCharacter, _animateColor, 0, _guideCharacter->strokes_size());
+		}
+
+		// Draw animating stroke
+		circle.setFillColor(_animateColor);
+		if (_animatingLines.size() > 0) {
+			for (size_t j = 0; j < _animatingLines.size(); j++) {
+				mainWindow->draw(_animatingLines[j]);
+				circle.setPosition((float)_traceCharacter->x(_animatingStroke, j), (float)_traceCharacter->y(_animatingStroke, j));
+				mainWindow->draw(circle);
+			}
 		}
 	}
 
-	// Draw strokes of current character
-	DrawStrokes(mainWindow, _character, _strokeColor, 0, _character->strokes_size());
-
-	mainWindow->SetView(view);
+	// Draw current character
+	circle.setFillColor(_strokeColor);
+	for (size_t i = 0; i < _strokeLines.size(); i++) {
+		circle.setPosition((float)_character->x(i, 0), (float)_character->y(i, 0));
+		mainWindow->draw(circle);
+		for (size_t j = 0; j < _strokeLines[i].size(); j++) {
+			mainWindow->draw(_strokeLines[i][j]);
+			circle.setPosition((float)_character->x(i, j+1), (float)_character->y(i, j+1));
+			mainWindow->draw(circle);
+		}
+	}
+	
+	mainWindow->setView(view);
 }
 
-void CharacterTile::Reclassify() {
+void CharacterTile::Reclassify () {
 	if (_currentStroke == 0) {
-		_unicode = sf::Unicode::Text();
+		_unicode = sf::String();
 		return;
 	}
 
@@ -400,17 +468,21 @@ void CharacterTile::Reclassify() {
 	delete result;
 }
 
-void CharacterTile::DrawStrokes(sf::RenderWindow *mainWindow, zinnia::Character *character, sf::Color color, size_t startStroke, size_t endStroke) {
+void CharacterTile::CreateLines(std::vector<std::vector<sf::RectangleShape>>& lines, zinnia::Character *character, sf::Color color, size_t startStroke, size_t endStroke) {
+	sf::RectangleShape rect;
+	rect.setFillColor(color);
+
 	for (size_t i = startStroke; i < endStroke; i++) {
-		float x2 = character->x(i, 0);
-		float y2 = character->y(i, 0);
+		sf::Vector2f p2((float)character->x(i, 0), (float)character->y(i, 0));
+
+		std::vector<sf::RectangleShape> stroke;
 		for (size_t j = 1; j < character->stroke_size(i); j++) {
-			float x1 = x2;
-			float y1 = y2;
-			x2 = character->x(i, j);
-			y2 = character->y(i, j);
-			mainWindow->Draw(sf::Shape::Line(x1, y1, x2, y2, _strokeThickness, color));
+			sf::Vector2f p1 = p2;
+			p2 = sf::Vector2f((float)character->x(i, j), (float)character->y(i, j));
+			sf::SetToLine(rect, p1, p2, _strokeThickness);
+			stroke.push_back(rect);
 		}
+		lines.push_back(stroke);
 	}
 }
 
--- Sun Magic/character_tile.h
+++ Sun Magic/character_tile.h
@@ -3,9 +3,6 @@
 #include "stdafx.h"
 #include "game_object.h"
 
-#include <iostream>
-#include <sstream>
-
 namespace SunMagic {
 
 class CharacterTile : GameObject {
@@ -13,27 +10,27 @@ class CharacterTile : GameObject {
 public:
 	// This must be called before using any Character Tiles
 	static void InitRecognizer(const char *modelFile);
-	static sf::Unicode::UTF32String UTF8ToUTF32(const char* utf8str);
+	static sf::String UTF8ToUTF32(const char* utf8str);
 
 	CharacterTile(float x = 0, float y = 0, size_t width = 300, size_t height = 300);
 	~CharacterTile();
 
 	// Get the character that the user has written so far
 	zinnia::Character * GetCharacter();
-	sf::Unicode::UTF32String GetUnicode();
+	sf::String GetUnicode();
 
 	size_t NumStrokes();
-	void AddStrokePoint(float x, float y);
+	void AddStrokePoint(sf::Vector2f p);
 	void EndStroke();
 	void UndoStroke();
 	void Clear();
 	void Resize(size_t width, size_t height);
 	float GetStrokeError(size_t stroke);
 
-	// The guide character is the character the user should be trying to write
-	zinnia::Character * GetGuideCharacter();
-	sf::Unicode::UTF32String GetGuideUnicode();
-	void SetGuideCharacter(zinnia::Character *character, sf::Unicode::UTF32String unicode);
+	// The trace character is the character the user should be trying to write
+	zinnia::Character * GetTraceCharacter();
+	sf::String GetTraceUnicode();
+	void SetTraceCharacter(zinnia::Character *character, sf::String unicode);
 	
 	// Setting the animation stroke to a valid stroke number will make the tile
 	// continuously animate that stroke
@@ -53,11 +50,11 @@ public:
 	sf::Color GetBorderColor();
 	void SetBorderColor(sf::Color color);
 
-	sf::Color GetGuideLineColor();
-	void SetGuideLineColor(sf::Color color);
-
 	sf::Color GetGuideColor();
 	void SetGuideColor(sf::Color color);
+
+	sf::Color GetTraceColor();
+	void SetTraceColor(sf::Color color);
 
 	sf::Color GetAnimateColor();
 	void SetAnimateColor(sf::Color color);
@@ -72,29 +69,31 @@ private:
 	static zinnia::Recognizer *_recognizer;
 
 	void Reclassify();
-	void DrawStrokes(sf::RenderWindow *mainWindow, zinnia::Character *character, sf::Color color, size_t startStroke, size_t endStroke);
+	void CreateLines(std::vector<std::vector<sf::RectangleShape>>& lines, zinnia::Character *character, sf::Color color, size_t startStroke, size_t endStroke);
 
 	zinnia::Character *_character;
-	sf::Unicode::UTF32String _unicode;
+	sf::String _unicode;
 	int _currentStroke;
 
-	zinnia::Character *_guideCharacter;
-	sf::Unicode::UTF32String _guideUnicode;
-	int _animationStroke;
+	zinnia::Character *_traceCharacter;
+	sf::String _traceUnicode;
+	int _animatingStroke;
 	float _animationSpeed;
 	float _waitSeconds;
 
 	float _strokeThickness;
 	sf::Color _borderColor;
-	sf::Color _guideLineColor;
 	sf::Color _guideColor;
+	sf::Color _traceColor;
 	sf::Color _animateColor;
 	sf::Color _strokeColor;
 
 	// Current animation variables
 	float _secondsToWait;
 	float _currentLineDistance;
-	std::vector<sf::Shape> _animationStrokeLines;
+	std::vector<std::vector<sf::RectangleShape>> _strokeLines;
+	std::vector<std::vector<sf::RectangleShape>> _traceLines;
+	std::vector<sf::RectangleShape> _animatingLines;
 };
 
 }
